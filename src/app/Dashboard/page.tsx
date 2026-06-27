383a492b34689f8650ca9e3d421e49bdff8fbfd9
Adarshhic <adarsh.mishradu0001@gmail.com>
2026-06-27 11:02:10 +0530

    session management

--- src/app/Dashboard/page.tsx
+++ src/app/Dashboard/page.tsx
@@ -1,10 +1,15 @@
-import React from   'react'
+import DashboardClient from '@/components/DashboardClient'
+import { getSession } from '@/lib/getSession'
+import React from 'react'
 
-function page(){
-    return (
-        <div>
-            Dashboard
-        </div>
-    )
+async function page() {
+    const session=await getSession()
+      if (!session?.user?.id) return null 
+  return (
+    <>
+      <DashboardClient ownerId={session.user.id!}/>
+    </>
+  )
 }
+
 export default page
